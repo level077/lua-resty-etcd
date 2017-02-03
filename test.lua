a1a1116fbc2f0a140b3b392c2c911b574b38d8b1
level077 <level077@163.com>
2017-02-03 15:41:19 +0800

    fix

--- test.lua
+++ test.lua
@@ -1,8 +1,9 @@
 local etcd = require "lua-resty-etcd.etcd"
 local _M = {}
+local handle
 function _M.init(conf)
-	_M.handle = etcd:new(conf)
-	local ok, err = _M["handle"]:init()
+	handle = etcd:new(conf)
+	local ok, err = handle:init()
 	if not ok then
 		return nil, err
 	end
@@ -10,11 +11,11 @@ function _M.init(conf)
 end
 
 function _M.status()
-	return _M["handle"]:status()
+	return handle:status()
 end
 
 function _M.set_config(action,key,value)
-	return _M["handle"]:set_config(action,key,value)
+	return handle:set_config(action,key,value)
 end
 
 return _M
